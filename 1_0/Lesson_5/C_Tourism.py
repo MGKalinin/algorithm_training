b295f5a319eaf5176255209435c2b8114f1577c1
MGKalinin <1953115@gmail.com>
2024-09-04 22:59:06 +0300

    add TotalHeight

--- 1_0/Lesson_5/C_Tourism.py
+++ 1_0/Lesson_5/C_Tourism.py
@@ -1,1 +1,35 @@
 # C. Туризм
+with open('input.txt', 'r') as file:
+    N = int(file.readline().strip())
+    chain = [tuple(map(int, file.readline().split())) for i in range(N)]
+    print(N, chain)
+    M = int(file.readline().strip())
+    track = [tuple(map(int, file.readline().split())) for k in range(M)]
+    print(M, track)
+
+from dataclasses import dataclass
+
+
+@dataclass
+class TotalHeight:
+    N: int
+    M: int
+    chain: list
+    track: list
+
+    def calculating_prefix_amount(self):
+        prefix_amount = [0] * (self.N + 1)
+        for j in range(1, self.N):
+            if self.chain[j][1] > self.chain[j - 1][1]:
+                prefix_amount[j] = prefix_amount[j-1] + (self.chain[j][1]-self.chain[j-1][1])
+            else:
+                prefix_amount[j] = prefix_amount[j-1]
+        # print(prefix_amount)
+        return prefix_amount
+
+    def result(self):
+
+
+
+ans = TotalHeight(N, M, chain, track)
+print(ans.calculating_prefix_amount())
