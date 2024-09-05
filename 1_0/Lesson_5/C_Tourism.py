2dc7088a8f4e9b783b245f1aeb9c7e807f4c633c
maxgp <1953115@gmail.com>
2024-09-05 09:53:20 +0300

    second start)

--- 1_0/Lesson_5/C_Tourism.py
+++ 1_0/Lesson_5/C_Tourism.py
@@ -17,19 +17,3 @@ class TotalHeight:
     chain: list
     track: list
 
-    def calculating_prefix_amount(self):
-        prefix_amount = [0] * (self.N + 1)
-        for j in range(1, self.N):
-            if self.chain[j][1] > self.chain[j - 1][1]:
-                prefix_amount[j] = prefix_amount[j-1] + (self.chain[j][1]-self.chain[j-1][1])
-            else:
-                prefix_amount[j] = prefix_amount[j-1]
-        # print(prefix_amount)
-        return prefix_amount
-
-    def result(self):
-
-
-
-ans = TotalHeight(N, M, chain, track)
-print(ans.calculating_prefix_amount())
