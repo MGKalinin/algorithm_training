c61a619ce3298aac2f448c0a0824dd6c23ae34b2
Попов Максим <1953115@gmail.com>
2024-09-16 21:08:13 +0300

    add bin_s lists

--- 1_0/Lesson_6/A_Binary_search.go
+++ 1_0/Lesson_6/A_Binary_search.go
@@ -2,16 +2,60 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
 	}
 	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	line := scanner.Text()
+	parts := strings.Fields(line)
+	N, err := strconv.Atoi(parts[0])
+	if err != nil {
+		fmt.Println(err)
+	}
+	K, err := strconv.Atoi(parts[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(N, K)
+
+	// список из N
+	scanner.Scan()
+	line = scanner.Text()
+	parts = strings.Fields(line)
+	arr_1 := make([]int, N)
+	for i, part := range parts {
+		arr_1[i], err = strconv.Atoi(part)
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
+	}
+	sort.Ints(arr_1)
+	fmt.Println(arr_1)
+	// список из K
+	scanner.Scan()
+	line = scanner.Text()
+	parts = strings.Fields(line)
+	arr_2 := make([]int, K)
+	for i, part := range parts {
+		arr_2[i], err = strconv.Atoi(part)
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
+	}
+	sort.Ints(arr_2)
+	fmt.Println(arr_2)
 
 }
