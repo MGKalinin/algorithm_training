757cf20b3fcb8591f110c31b41488dab8799b9d9
MG_Kalinin <1953115@gmail.com>
2024-11-15 14:42:36 +0300

    add 88. Merge Sorted Array

--- main.go
+++ main.go
@@ -1,45 +1,13 @@
 package main
 
-import "fmt"
-
-// 238. Product of Array Except Self
+// 88. Merge Sorted Array
 func main() {
-	nums := []int{1, 2, 3, 4}
-	n := len(nums)
-	result := make([]int, n)
-	prefix := prefixProduct(nums)
-	suffix := suffixProduct(nums)
-	for i := 0; i < n; i++ {
-		if i == 0 {
-			result[i] = suffix[i+1]
-		} else if i == n-1 {
-			result[n-1] = prefix[i-1]
-		} else {
-			result[i] = prefix[i-1] * suffix[i+1]
-		}
-	}
-	fmt.Println(result)
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
 }
 
-// функция вычисляет перфиксное произведение
-func prefixProduct(nums []int) []int {
-	n := len(nums)
-	prefixProd := make([]int, n)
-	prefixProd[0] = nums[0]
-	for i := 1; i < n; i++ {
-		prefixProd[i] = prefixProd[i-1] * nums[i]
-	}
-	// fmt.Println(prefix_prod)
-	return prefixProd
-}
-
-// функция вычисляет суффиксное произведение
-func suffixProduct(nums []int) []int {
-	n := len(nums)
-	suffixProd := make([]int, n)
-	suffixProd[n-1] = nums[n-1]
-	for i := (n - 2); i >= 0; i-- {
-		suffixProd[i] = nums[i] * suffixProd[i+1]
-	}
-	return suffixProd
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	return
 }
