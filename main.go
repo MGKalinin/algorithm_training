269e1f1a773b896c6cf3aa11881cd4956a29a4ae
MG_Kalinin <1953115@gmail.com>
2024-11-26 20:43:35 +0300

    add some methods

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // стек
 type Stack struct {
@@ -11,7 +17,7 @@ type Stack struct {
 метод push n
 Добавить в стек число n (значение n задается после команды). Программа должна вывести ok.
 */
-func (s *Stack) Push(command string, element int) {
+func (s *Stack) Push(element int) {
 	s.elements = append(s.elements, element)
 	fmt.Println("ok")
 }
@@ -19,15 +25,69 @@ func (s *Stack) Push(command string, element int) {
 /*
 метод pop
 Удалить из стека последний элемент. Программа должна вывести его значение.
+Если во входных данных встречается операция back или pop, и при этом стек пуст,
+то программа должна вместо числового значения вывести строку error.
 */
-func (s *Stack) Pop(command string) {
+func (s *Stack) Pop() {
 	n := len(s.elements)
-	ans := s.elements[n-1]
-	s.elements = s.elements[:(n - 1)]
-	fmt.Println(ans)
+	if n == 0 {
+		fmt.Println("error")
+	} else {
+		ans := s.elements[n-1]
+		s.elements = s.elements[:(n - 1)]
+		fmt.Println(ans)
+	}
+}
+
+/*
+back
+Программа должна вывести значение последнего элемента, не удаляя его из стека.
+Если во входных данных встречается операция back или pop, и при этом стек пуст,
+то программа должна вместо числового значения вывести строку error.
+*/
+func (s *Stack) Back() {
+
+}
+
+/*
+size
+Программа должна вывести количество элементов в стеке.
+*/
+func (s *Stack) Size() {
+
+}
+
+/*
+clear
+Программа должна очистить стек и вывести ok.
+*/
+func (s *Stack) Clear() {
+
+}
+
+/*
+exit
+Программа должна вывести bye и завершить работу.
+*/
+func (s *Stack) Exit() {
+
 }
 
 // 140. Стек с защитой от ошибок
 func main() {
+	stack := Stack{}
+	// * строку --
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		command := scanner.Text()
+		parts := strings.Fields(command)
+		switch parts[0] {
+		case "push":
+			num, _ := strconv.Atoi(parts[1])
+			stack.Push(num)
+		case "pop":
+			stack.Pop()
+		}
+	}
 
 }
