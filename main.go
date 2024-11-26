7c158f7379eda2a51e1a53b077a9f445f8c03fc5
MG_Kalinin <1953115@gmail.com>
2024-11-26 21:10:18 +0300

    add first var

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func (s *Stack) Pop() {
 	n := len(s.elements)
 	if n == 0 {
 		fmt.Println("error")
+		return
 	} else {
 		ans := s.elements[n-1]
 		s.elements = s.elements[:(n - 1)]
@@ -46,7 +47,14 @@ back
 то программа должна вместо числового значения вывести строку error.
 */
 func (s *Stack) Back() {
-
+	n := len(s.elements)
+	if n == 0 {
+		fmt.Println("error")
+		return
+	} else {
+		ans := s.elements[n-1]
+		fmt.Println(ans)
+	}
 }
 
 /*
@@ -54,7 +62,8 @@ size
 Программа должна вывести количество элементов в стеке.
 */
 func (s *Stack) Size() {
-
+	n := len(s.elements)
+	fmt.Println(n)
 }
 
 /*
@@ -62,7 +71,8 @@ clear
 Программа должна очистить стек и вывести ok.
 */
 func (s *Stack) Clear() {
-
+	s.elements = nil
+	fmt.Println("ok")
 }
 
 /*
@@ -70,7 +80,8 @@ exit
 Программа должна вывести bye и завершить работу.
 */
 func (s *Stack) Exit() {
-
+	fmt.Println("bye")
+	return
 }
 
 // 140. Стек с защитой от ошибок
@@ -87,6 +98,16 @@ func main() {
 			stack.Push(num)
 		case "pop":
 			stack.Pop()
+		case "back":
+			stack.Back()
+		case "size":
+			stack.Size()
+		case "clear":
+			stack.Clear()
+		case "exit":
+			stack.Exit()
+			return
+
 		}
 	}
 
