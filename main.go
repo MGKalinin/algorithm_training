1d199464a3b799feeb8b25382ea834e87643211f
MG_Kalinin <1953115@gmail.com>
2024-11-27 13:05:02 +0300

    add scan

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 // 141. Правильная скобочная последовательность
 func main() {
-
+	// *строку--
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
