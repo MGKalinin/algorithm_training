bf114246e1218d7fda504b3e9b64d4a401a397de
MG_Kalinin <1953115@gmail.com>
2024-12-01 18:45:09 +0300

    start 2390. Removing Stars From a String

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
-// 20. Valid Parentheses
+package main
+
+import "fmt"
+
+// LeetCode 75
+// Стеки / Stack
+
+// 2390. Removing Stars From a String
 func main() {
-
+	arr := "leet**cod*e"
+	removeStars(arr)
 }
-func isValid(s string) bool {
 
+type Stack struct {
+	elements []string
+}
+
+func (s *Stack) Push(elements []string, elem string) {
+	elements = append(elements, elem)
+}
+
+func removeStars(s string) string {
+	stack := Stack{}
+	for _, char := range s {
+		stack.Push(stack.elements, char)
+	}
+	fmt.Println(stack)
+	if len(stack.elements) != 0 {
+		return true
+	}
+	return false
 }
