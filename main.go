c054a9457d0eadd6a750267f2f286cf5a36c0065
MG_Kalinin <1953115@gmail.com>
2024-12-15 04:29:53 +0300

    add Largest Prime Factor

--- main.go
+++ main.go
@@ -1,28 +1,38 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
+
+// Largest Prime Factor https://projecteuler.net/problem=3
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	var lines []string
-
-	// Читаем строки до конца ввода или до определенного количества строк
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
-		lines = append(lines, line)
-	}
-
-	// Выводим прочитанные строки
-	for _, line := range lines {
-		fmt.Print(line)
-	}
+	/* Простые множители числа 13195 — это 5 , 7 , 13 и 29
+	Какой самый большой простой множитель числа 600851475143 ? */
+	findResult(13195)
 }
 
-//
+// функция нахождения простых чисел и простых множителей,
+// и возвращает максимальный простой множитель числа
+func findResult(n int) int {
+	prNum := []int{}
+	for i := 0; i*i < n; i++ {
+		if i <= 1 {
+			continue
+		}
+		if i <= 3 {
+			prNum = append(prNum, i)
+		}
+		if i%2 == 0 || i%3 == 0 {
+			continue
+		} else {
+			prNum = append(prNum, i)
+		}
+	}
+	fmt.Println(prNum)
+	prDelitel := []int{}
+	for _, num := range prNum {
+		for n % num {
+			prDelitel = append(prDelitel, num)
+
+		}
+	}
+}
