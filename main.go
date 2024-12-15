618ecb3f350feccc249bf57a609d32f20c31ae9c
MG_Kalinin <1953115@gmail.com>
2024-12-15 19:27:29 +0300

    decided 	Largest Prime Factor

--- main.go
+++ main.go
@@ -7,32 +7,40 @@ import "fmt"
 func main() {
 	/* Простые множители числа 13195 — это 5 , 7 , 13 и 29
 	Какой самый большой простой множитель числа 600851475143 ? */
-	findResult(13195)
+	findRes(600851475143)
 }
 
 // функция нахождения простых чисел и простых множителей,
 // и возвращает максимальный простой множитель числа
-func findResult(n int) int {
-	prNum := []int{}
-	for i := 0; i*i < n; i++ {
-		if i <= 1 {
-			continue
-		}
-		if i <= 3 {
-			prNum = append(prNum, i)
-		}
-		if i%2 == 0 || i%3 == 0 {
-			continue
-		} else {
-			prNum = append(prNum, i)
-		}
+func primeNumber(n int) bool {
+	if n <= 1 {
+		return false
 	}
-	fmt.Println(prNum)
-	prDelitel := []int{}
-	for _, num := range prNum {
-		for n % int(num) {
-			prDelitel = append(prDelitel, num)
+	if n <= 3 {
+		return true
+	}
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+	return true
+}
 
+func findRes(n int) {
+	ans := []int{}
+	for i := 2; i <= n; i++ {
+		if primeNumber(i) && n%i == 0 {
+			ans = append(ans, i)
+			for n%i == 0 {
+				n /= i
+			}
 		}
 	}
+	fmt.Println(ans)
+	maxelem := ans[0]
+	for _, num := range ans {
+		if num > maxelem {
+			maxelem = num
+		}
+	}
+	fmt.Println(maxelem)
 }
