6168fe12fa91cccead4f083b9c20bfe00b6c6877
MG_Kalinin <1953115@gmail.com>
2024-12-17 21:28:30 +0300

    add second try

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 )
 
 // B. Кольцевая линия метро https://contest.yandex.ru/contest/28730/problems/B/
@@ -15,11 +14,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	first := math.Abs(float64(i - j))
-	one := math.Abs(float64(N - i))
-	second := math.Abs(float64(N - j))
-	three := one + second
+	first := j - i
+	one := (N - i)
+	second := (N - j)
 	fmt.Println(first)
-	fmt.Println(three)
+	fmt.Println(one)
+	fmt.Println(second)
 
 }
