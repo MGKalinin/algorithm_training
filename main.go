791209d402cbfcfea77c7ed64d3c1b7775ac1abe
MG_Kalinin <1953115@gmail.com>
2024-12-17 21:13:59 +0300

    add B. Кольцевая линия метро

--- main.go
+++ main.go
@@ -1,46 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-// Largest Prime Factor https://projecteuler.net/problem=3
+// B. Кольцевая линия метро https://contest.yandex.ru/contest/28730/problems/B/
 
 func main() {
-	/* Простые множители числа 13195 — это 5 , 7 , 13 и 29
-	Какой самый большой простой множитель числа 600851475143 ? */
-	findRes(600851475143)
-}
+	// * число --
+	var N, i, j int64
+	_, err := fmt.Scan(&N, &i, &j)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// функция нахождения простых чисел и простых множителей,
-// и возвращает максимальный простой множитель числа
-func primeNumber(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	if n <= 3 {
-		return true
-	}
-	if n%2 == 0 || n%3 == 0 {
-		return false
-	}
-	return true
-}
-
-func findRes(n int) {
-	ans := []int{}
-	for i := 2; i <= n; i++ {
-		if primeNumber(i) && n%i == 0 {
-			ans = append(ans, i)
-			for n%i == 0 {
-				n /= i
-			}
-		}
-	}
-	fmt.Println(ans)
-	maxelem := ans[0]
-	for _, num := range ans {
-		if num > maxelem {
-			maxelem = num
-		}
-	}
-	fmt.Println(maxelem)
 }
