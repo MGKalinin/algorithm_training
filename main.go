d97fec065f9a75fd1aac26ca4188033e7fee2d58
MG_Kalinin <1953115@gmail.com>
2024-12-22 18:25:01 +0300

    add C. Даты

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 	"log"
 )
 
-// B. Кольцевая линия метро https://contest.yandex.ru/contest/28730/problems/B/
-// 10 1 9
-// 20 10 8 (тест №8)
+// C. Даты https://contest.yandex.ru/contest/28730/problems/C/
+
 func main() {
 	// * число --
 	var N, i, j int
@@ -15,34 +14,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println(N)
-	// fmt.Println(i)
-	// fmt.Println(j)
-	if i < j {
-		i, j = i, j
-	} else {
-		i, j = j, i
-	}
 
-	a := 0
-	for k := i + 1; k < j; k++ {
-
-		a++ //между станциями
-	}
-	// fmt.Println(a)
-	b := 0
-	for k := 1; k < i; k++ {
-		b++ //до i
-	}
-	// fmt.Println(b)
-
-	for k := j; k < N; k++ {
-		b++ //от j до конца
-	}
-	// fmt.Println(b)
-	if a < b {
-		fmt.Println(a)
-	} else {
-		fmt.Println(b)
-	}
 }
