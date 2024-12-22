2fba48a810760da92860b9e610c794d3c80b96b3
MG_Kalinin <1953115@gmail.com>
2024-12-22 10:37:56 +0300

    add fff

--- main.go
+++ main.go
@@ -18,18 +18,16 @@ func main() {
 	fmt.Println(i)
 	fmt.Println(j)
 	totalStations := []int{} //всего станций
-	for i := 0; i <= N; i++ {
+	for i := 1; i <= N; i++ {
 		totalStations = append(totalStations, i)
 	}
-	betweenStations := totalStations[i:j]
-	betweenStationsCircle1 := totalStations[:i]
-	betweenStationsCircle2 := totalStations[j:]
+	between := totalStations[i:j]
+	part1 := totalStations[:i]
+	part2 := totalStations[j:]
 
-	for _, num := range betweenStationsCircle1 {
-		betweenStationsCircle2 = append(betweenStationsCircle2, num)
-	}
 	fmt.Println(totalStations)
-	fmt.Println(betweenStations)
-	fmt.Println(betweenStationsCircle1)
+	fmt.Println(between)
+	fmt.Println(part1)
+	fmt.Println(part2)
 
 }
