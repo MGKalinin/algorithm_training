7d238c74d0504bd6111112153f46bbf0931d3ed2
MG_Kalinin <1953115@gmail.com>
2024-12-29 19:27:03 +0300

    add B. Дома и магазины

--- main.go
+++ main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-)
-
-// A. Количество равных максимальному https://contest.yandex.ru/contest/28738/problems/A/
+// B. Дома и магазины https://contest.yandex.ru/contest/28738/problems/B/
 
 func main() {
-	arr := []int{}
-	// * строку --
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		num, err := strconv.Atoi(line)
-		if err != nil {
-			log.Printf("Ошибка преобразования '%s' в число: %v, пропускаем.", line, err)
-			continue
-		}
-		if num == 0 {
-			break
-		}
-		arr = append(arr, num)
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("Ошибка чтения из stdin: %v", err)
-	}
-	// fmt.Println(arr)
-	// посчитать к-во чисел равных максимальному
-	count := 1
-	maxEl := arr[0]
-	for i := 1; i < len(arr); i++ {
-		if arr[i] > maxEl {
-			maxEl = arr[i]
-			count = 1
-		} else if arr[i] == maxEl {
-			count++
-		}
-	}
-	fmt.Println(count)
+
 }
