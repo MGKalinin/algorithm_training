f43ff6dc69d5791817380db331d83d1181f9c513
MG_Kalinin <1953115@gmail.com>
2024-12-30 19:45:43 +0300

    + A. Префиксные суммы bufio.NewReader(os.Stdin)

--- main.go
+++ main.go
@@ -11,51 +11,31 @@ import (
 // A. Префиксные суммы https://contest.yandex.ru/contest/29075/problems/A/
 
 func main() {
-	// * строку --n и q
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	line := scanner.Text()
-	parts := strings.Split(line, " ")
-	n, _ := strconv.Atoi(parts[0]) //размер массива
-	q, _ := strconv.Atoi(parts[1]) //количество запросов
-	//массив размера n
-	// * массив чисел известной длины --
-	var arr = make([]int, n)
-	scanner.Scan()
-	line = scanner.Text()
-	parts = strings.Split(line, " ")
-	for i := 0; i < n; i++ {
-		arr[i], _ = strconv.Atoi(parts[i])
-	}
-	// Вычисление префиксных сумм
-	prefixSums := make([]int, n+1)
+	reader := bufio.NewReader(os.Stdin)
+	// считать размер и количество запросов
+	line, _ := reader.ReadString('\n')
+	parts := strings.Fields(line) //получается слайс строк без пробелов и символов новой строки
+	n, _ := strconv.Atoi(parts[0])
+	q, _ := strconv.Atoi(parts[1])
+	// чтение массива и построение префиксных сумм
+	line, _ = reader.ReadString('\n')
+	parts = strings.Fields(line)
+	prefixSum := make([]int, n+1)
 	for i := 1; i <= n; i++ {
-		prefixSums[i] = prefixSums[i-1] + arr[i-1]
+		num, _ := strconv.Atoi(parts[i-1])
+		prefixSum[i] = prefixSum[i-1] + num
 	}
-	// fmt.Println("\n", prefixSums)
-	// fmt.Println(n, q)
-	// fmt.Println(arr)
-	//количество запросов l, r
+	//подсчёт префиксных сумм в заданном диапазоне
+	var result strings.Builder
 	for i := 0; i < q; i++ {
-		scanner.Scan()
-		line = scanner.Text()
-		parts = strings.Split(line, " ")
-		l, _ := strconv.Atoi(parts[0]) //левая граница отрезка
-		r, _ := strconv.Atoi(parts[1]) //правая граница отрезка
-		// fmt.Println("---", l, r)
-		// Подсчет суммы на отрезке [l, r]
-		sum := prefixSums[r] - prefixSums[l-1]
-		fmt.Println(sum)
+		line, _ = reader.ReadString('\n')
+		parts = strings.Fields(line)
+		l, _ := strconv.Atoi(parts[0])
+		r, _ := strconv.Atoi(parts[1])
+
+		sum := prefixSum[r] - prefixSum[l-1]
+		result.WriteString(strconv.Itoa(sum) + "\n")
 
 	}
+	fmt.Print(result.String())
 }
-
-// функция подсчёта префиксной суммы в указанном диапазоне
-// прикрутить тестирование функции prefixSum?+
-// func prefixSum(arr []int, n, l, r int) int {
-// 	sum := make([]int, n+1)
-// 	for i := 1; i <= n; i++ {
-// 		sum[i] = sum[i-1] + arr[i-1]
-// 	}
-// 	return sum[r] - sum[l-1]
-// }
