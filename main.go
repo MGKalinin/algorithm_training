385e15ec5e9f00bbf01b0bc3ceec4f19666d2eea
MG_Kalinin <1953115@gmail.com>
2024-12-30 21:28:02 +0300

    fail 4 test

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,15 +15,26 @@ func main() {
 	// считать размер и количество запросов
 	line, _ := reader.ReadString('\n')
 	parts := strings.Fields(line) //получается слайс строк без пробелов и символов новой строки
-	// n, _ := strconv.Atoi(parts[0])
-	// q, _ := strconv.Atoi(parts[1])
-	// // чтение массива и построение префиксных сумм
-	// line, _ = reader.ReadString('\n')
-	// parts = strings.Fields(line)
-	// prefixSum := make([]int, n+1)
-	// for i := 1; i <= n; i++ {
-	// 	num, _ := strconv.Atoi(parts[i-1])
-	// 	prefixSum[i] = prefixSum[i-1] + num
-	// }
+	n, _ := strconv.Atoi(parts[0])
+	// чтение массива
+	line, _ = reader.ReadString('\n')
+	parts = strings.Fields(line)
+	// подсчёт префиксной суммы
+	maxResult, _ := strconv.Atoi(parts[0])
+	prefixSum := make([]int, n+1)
 
+	for i := 1; i <= n; i++ {
+		num, _ := strconv.Atoi(parts[i-1])
+		prefixSum[i] = prefixSum[i-1] + num
+		if prefixSum[i] > maxResult {
+			maxResult = prefixSum[i]
+
+		} else {
+			continue
+		}
+	}
+	fmt.Println(maxResult)
 }
+
+// 5
+// 450402558 -840167367 -231820501 586187125 -627664644
