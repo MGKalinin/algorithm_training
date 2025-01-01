25851789852dbfaa6510c338202c177b20fdf099
MG_Kalinin <1953115@gmail.com>
2025-01-01 21:14:00 +0300

    fail test 5

--- main.go
+++ main.go
@@ -8,58 +8,60 @@ import (
 	"strings"
 )
 
-// B. Максимальная сумма https://contest.yandex.ru/contest/29075/problems/B/
+// C. Каждому по компьютеру https://contest.yandex.ru/contest/29075/problems/C/
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// считать размер массива
 	line, _ := reader.ReadString('\n')
-	parts := strings.Fields(line) //получается слайс строк без пробелов и символов новой строки
-	n, _ := strconv.Atoi(parts[0])
+	parts := strings.Fields(line)  //получается слайс строк без пробелов и символов новой строки
+	N, _ := strconv.Atoi(parts[0]) //N групп
+	M, _ := strconv.Atoi(parts[1]) //M аудиторий
 	// чтение массива
 	line, _ = reader.ReadString('\n')
 	parts = strings.Fields(line)
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		arr[i], _ = strconv.Atoi(parts[i])
+	groupN := make([]int, N) //слайс челов по группам
+	for i := 0; i < N; i++ {
+		groupN[i], _ = strconv.Atoi(parts[i])
 	}
-	// подсчёт префиксных сумм всего массива
-	prefixSumm := make([]int, n+1)
-	for i := 1; i <= n; i++ {
-		prefixSumm[i] = prefixSumm[i-1] + arr[i-1]
+	//слайс компьютеров в аудиториях
+	line, _ = reader.ReadString('\n')
+	parts = strings.Fields(line)
+	roomsM := make([]int, M)
+	for i := 0; i < M; i++ {
+		roomsM[i], _ = strconv.Atoi(parts[i])
 	}
-	// сравнить разницу минимальной префиксной суммы и максимальной префиксной суммы
-	maxPrefixSum := arr[0] //максимальная префиксная сумма
-	minPrefixSum := 0      //минимальнаЯ префиксная сумма
-	for i := 1; i <= n; i++ {
-		maxPrefixSum = calcMax(maxPrefixSum, prefixSumm[i]-minPrefixSum) // максимум среди
-		// текущего максимума и разностью минимальной префиксной суммы и
-		// текущего значения в префиксных суммах
-		minPrefixSum = calcMin(minPrefixSum, prefixSumm[i]) //мимнмальная префиксная сумма это
-		// минимум между текущей минимальной префиксной суммой и
-		// текущим значением в списке префиксных сумм
 
+	// Выведите на первой строке число P - количество групп, которые удастся распределить по аудиториям
+	// На второй строке выведите распределение групп по аудиториям – N чисел,
+	// i-е число должно соответствовать номеру аудитории, в которой должна заниматься i-я группа.
+	// Если i-я группа осталась без аудитории, i-е число должно быть равно 0
+	// M>=N
+
+	// N := 10
+	// M := 10
+	// groupN := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1} // слайс  групп
+	// roomsM := []int{2, 2, 2, 2, 2, 1, 2, 2, 2, 2} // слайс аудиторий
+	P := 0
+	ans := []int{}
+	for i := 0; i < M; i++ {
+		if groupN[i] < roomsM[i] { //если группа меньше компьютеров в аудитории добавить в слай ответов номер аудитории
+			ans = append(ans, i+1)
+			P++
+		} else if groupN[i] >= roomsM[i] { //группа равна либо больше компьютеров в аудитории добавить 0
+			ans = append(ans, 0)
+		}
 	}
-	fmt.Println(maxPrefixSum)
+
+	fmt.Println(P)
+	// fmt.Println(ans)
+	for _, val := range ans {
+		fmt.Println(val)
+	}
 }
 
-// 5
-// 450402558 -840167367 -231820501 586187125 -627664644
-
-// функция выбора максимального значения
-func calcMax(a int, b int) int {
-	if a > b {
-		return a
-	} else {
-		return b
-	}
-}
-
-// функция выбора минимального значения
-func calcMin(a int, b int) int {
-	if a < b {
-		return a
-	} else {
-		return b
-	}
-}
+// тест 4
+// 10 10 N групп, M аудиторий
+// 1 1 1 1 1 1 1 1 1 1 человек в каждой группе
+// 2 2 2 2 2 2 2 2 2 1 компьютеров в каждой аудитории
+// какое кол-во групп разместить? +один комп для учителя
