ca9f94e449dc96a6c06138d71ac056dafd77d207
MG_Kalinin <1953115@gmail.com>
2025-01-02 22:12:29 +0300

    test 5 failed

--- main.go
+++ main.go
@@ -1,36 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 // C. Каждому по компьютеру https://contest.yandex.ru/contest/29075/problems/C/
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	// считать размер массива
-	line, _ := reader.ReadString('\n')
-	parts := strings.Fields(line)  //получается слайс строк без пробелов и символов новой строки
-	N, _ := strconv.Atoi(parts[0]) //N групп
-	M, _ := strconv.Atoi(parts[1]) //M аудиторий
-	// чтение массива
-	line, _ = reader.ReadString('\n')
-	parts = strings.Fields(line)
-	groupN := make([]int, N) //слайс челов по группам
-	for i := 0; i < N; i++ {
-		groupN[i], _ = strconv.Atoi(parts[i])
-	}
-	//слайс компьютеров в аудиториях
-	line, _ = reader.ReadString('\n')
-	parts = strings.Fields(line)
-	roomsM := make([]int, M)
-	for i := 0; i < M; i++ {
-		roomsM[i], _ = strconv.Atoi(parts[i])
-	}
+	// reader := bufio.NewReader(os.Stdin)
+	// // считать размер массива
+	// line, _ := reader.ReadString('\n')
+	// parts := strings.Fields(line)  //получается слайс строк без пробелов и символов новой строки
+	// N, _ := strconv.Atoi(parts[0]) //N групп
+	// M, _ := strconv.Atoi(parts[1]) //M аудиторий
+	// // чтение массива
+	// line, _ = reader.ReadString('\n')
+	// parts = strings.Fields(line)
+	// groupN := make([]int, N) //слайс челов по группам
+	// for i := 0; i < N; i++ {
+	// 	groupN[i], _ = strconv.Atoi(parts[i])
+	// }
+	// //слайс компьютеров в аудиториях
+	// line, _ = reader.ReadString('\n')
+	// parts = strings.Fields(line)
+	// roomsM := make([]int, M)
+	// for i := 0; i < M; i++ {
+	// 	roomsM[i], _ = strconv.Atoi(parts[i])
+	// }
 
 	// Выведите на первой строке число P - количество групп, которые удастся распределить по аудиториям
 	// На второй строке выведите распределение групп по аудиториям – N чисел,
@@ -38,18 +34,38 @@ func main() {
 	// Если i-я группа осталась без аудитории, i-е число должно быть равно 0
 	// M>=N
 
-	// N := 10
-	// M := 10
-	// groupN := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1} // слайс  групп
-	// roomsM := []int{2, 2, 2, 2, 2, 1, 2, 2, 2, 2} // слайс аудиторий
+	N := 3
+	M := 3
+	groupN := []int{1, 2, 3} // слайс  групп
+	roomsM := []int{3, 4, 2} // слайс аудиторий
+
+	//возможность разместить
+	abilityPlace := func(roomsSize, groupSize int) bool {
+		return roomsSize >= groupSize+1
+	}
+
 	P := 0
-	ans := []int{}
-	for i := 0; i < M; i++ {
-		if groupN[i] < roomsM[i] { //если группа меньше компьютеров в аудитории добавить в слай ответов номер аудитории
-			ans = append(ans, i+1)
-			P++
-		} else if groupN[i] >= roomsM[i] { //группа равна либо больше компьютеров в аудитории добавить 0
-			ans = append(ans, 0)
+	ans := make([]int, N)
+	for indGroup := 0; indGroup < N; indGroup++ {
+		fmt.Println("indGroup", indGroup)
+		var foundGroup bool
+		for indRooms := 0; indRooms < M; indRooms++ {
+			fmt.Println("indRooms", indRooms)
+			if abilityPlace(roomsM[indRooms], groupN[indGroup]) {
+				ans[indGroup] = indRooms + 1
+				P++
+				foundGroup = true
+				// добавить поиск наименьшего значения
+				// аудитории
+
+				// заменить значение аудитории
+				// где размещена группа  на 0
+				roomsM[indRooms] = 0
+				break
+			}
+		}
+		if !foundGroup {
+			ans[indGroup] = 0
 		}
 	}
 
@@ -65,3 +81,8 @@ func main() {
 // 1 1 1 1 1 1 1 1 1 1 человек в каждой группе
 // 2 2 2 2 2 2 2 2 2 1 компьютеров в каждой аудитории
 // какое кол-во групп разместить? +один комп для учителя
+
+// тест 5
+// 3 3
+// 1 2 3
+// 3 4 2
