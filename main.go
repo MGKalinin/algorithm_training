25f1166d1d3be5923b5388d472715e174e2f219c
MG_Kalinin <1953115@gmail.com>
2025-01-03 04:13:34 +0300

    start

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 // D. Правильная, круглая, скобочная https://contest.yandex.ru/contest/29075/problems/D/
@@ -13,22 +12,21 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// считать размер массива
 	line, _ := reader.ReadString('\n')
-	parts := strings.Fields(line)  //получается слайс строк без пробелов и символов новой строки
-	N, _ := strconv.Atoi(parts[0]) //N групп
-	M, _ := strconv.Atoi(parts[1]) //M аудиторий
-	// чтение массива
-	line, _ = reader.ReadString('\n')
-	parts = strings.Fields(line)
-	groupN := make([]int, N) //слайс челов по группам
-	for i := 0; i < N; i++ {
-		groupN[i], _ = strconv.Atoi(parts[i])
+	// parts := strings.Fields(line) //получается слайс строк без пробелов и символов новой строки
+	// N, _ := strconv.Atoi(parts[0]) //N групп
+	// M, _ := strconv.Atoi(parts[1]) //M аудиторий
+	// // чтение массива
+	// line, _ = reader.ReadString('\n')
+	// parts = strings.Fields(line)
+	// groupN := make([]int, N) //слайс челов по группам
+	// for i := 0; i < N; i++ {
+	// 	groupN[i], _ = strconv.Atoi(parts[i])
+	// }
+	fmt.Println(line)
+	line = line[:len(line)-1]
+	for _, ch := range line {
+		if ch == '(' {
+			fmt.Println(ch)
+		}
 	}
-	//слайс компьютеров в аудиториях
-	line, _ = reader.ReadString('\n')
-	parts = strings.Fields(line)
-	roomsM := make([]int, M)
-	for i := 0; i < M; i++ {
-		roomsM[i], _ = strconv.Atoi(parts[i])
-	}
-
 }
