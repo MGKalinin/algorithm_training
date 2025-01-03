2ececa5960a8c438b96db747fc0bc0ca73f12569
MG_Kalinin <1953115@gmail.com>
2025-01-04 01:17:25 +0300

    + D. Правильная, круглая, скобочная

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // D. Правильная, круглая, скобочная https://contest.yandex.ru/contest/29075/problems/D/
@@ -12,21 +13,26 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// считать размер массива
 	line, _ := reader.ReadString('\n')
-	// parts := strings.Fields(line) //получается слайс строк без пробелов и символов новой строки
-	// N, _ := strconv.Atoi(parts[0]) //N групп
-	// M, _ := strconv.Atoi(parts[1]) //M аудиторий
-	// // чтение массива
-	// line, _ = reader.ReadString('\n')
-	// parts = strings.Fields(line)
-	// groupN := make([]int, N) //слайс челов по группам
-	// for i := 0; i < N; i++ {
-	// 	groupN[i], _ = strconv.Atoi(parts[i])
-	// }
-	fmt.Println(line)
-	line = line[:len(line)-1]
+
+	line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
+	stack := []rune{}
 	for _, ch := range line {
 		if ch == '(' {
-			fmt.Println(ch)
+			stack = append(stack, ch)
+
+		} else if ch == ')' {
+			if len(stack) == 0 {
+				fmt.Println("NO")
+				return
+			}
+			stack = stack[:len(stack)-1]
 		}
+	}
+	if len(stack) == 0 {
+		fmt.Println("YES")
+		// return "YES"
+	} else {
+		fmt.Println("NO")
+		// return "NO"
 	}
 }
