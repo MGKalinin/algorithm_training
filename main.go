de56793a06ab2c0edd443d4f8f1bb074871f372e
MG_Kalinin <1953115@gmail.com>
2025-01-04 01:21:21 +0300

    add E. Сумма трёх

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strings"
 )
 
-// D. Правильная, круглая, скобочная https://contest.yandex.ru/contest/29075/problems/D/
+// E. Сумма трёх https://contest.yandex.ru/contest/29075/problems/E/
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -15,24 +14,5 @@ func main() {
 	line, _ := reader.ReadString('\n')
 
 	line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
-	stack := []rune{}
-	for _, ch := range line {
-		if ch == '(' {
-			stack = append(stack, ch)
 
-		} else if ch == ')' {
-			if len(stack) == 0 {
-				fmt.Println("NO")
-				return
-			}
-			stack = stack[:len(stack)-1]
-		}
-	}
-	if len(stack) == 0 {
-		fmt.Println("YES")
-		// return "YES"
-	} else {
-		fmt.Println("NO")
-		// return "NO"
-	}
 }
