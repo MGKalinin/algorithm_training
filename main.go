784e93b9d28793d24716d80b47216ee613a7c8b3
MG_Kalinin <1953115@gmail.com>
2025-01-06 01:38:27 +0300

    add 1768. Merge Strings Alternately

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
-import (
-	"bufio"
-	"os"
-	"strings"
-)
+import "fmt"
 
-// E. Сумма трёх https://contest.yandex.ru/contest/29075/problems/E/
+// 1768. Merge Strings Alternately
+// https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	// считать размер массива
-	line, _ := reader.ReadString('\n')
+	// reader := bufio.NewReader(os.Stdin)
+	// // считать размер массива
+	// line, _ := reader.ReadString('\n')
 
-	line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
-
+	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
+	// fmt.Println(line)
+	word1 := "abc"
+	word2 := "pqr"
+	// Output: "apbqcr"
+	fmt.Println(mergeAlternately(word1, word2))
+}
+func mergeAlternately(word1 string, word2 string) string {
+	var word3 string
+	if len(word1) == len(word2) {
+		for i, _ := range word1 {
+			for k, _ := range word2 {
+				// word3 = word1[i] + word2[k]
+			}
+		}
+	}
+	return word3
 }
