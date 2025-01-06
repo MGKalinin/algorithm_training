925eb1ec991de5ba5a848fa4b5fda780353d4ba8
MG_Kalinin <1953115@gmail.com>
2025-01-07 02:45:53 +0300

    added A. Быстрый поиск в массиве

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
-
-// 1071. Greatest Common Divisor of Strings
-// https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75
+// A. Быстрый поиск в массиве
+// https://contest.yandex.ru/contest/29188/problems/A/
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
 	// // считать размер массива
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
-
-	str1 := "ABCABC"
-	str2 := "ABC"
-	// Output: "ABC"
-	// str1 := "ABABAB"
-	// str2 := "ABAB"
-	// Output: "AB"
-	fmt.Println(gcdOfStrings(str1, str2))
-}
-func gcdOfStrings(str1 string, str2 string) string {
 
 }
