2951d8c929840ca0c2392fb423f3ce01d669ab3d
MG_Kalinin <1953115@gmail.com>
2025-01-09 00:08:35 +0300

    add 605. Can Place Flowers

--- main.go
+++ main.go
@@ -2,27 +2,20 @@ package main
 
 import "fmt"
 
-// 1431. Kids With the Greatest Number of Candies
-// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75
+// 605. Can Place Flowers
+// https://leetcode.com/problems/can-place-flowers/description/?envType=study-plan-v2&envId=leetcode-75
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
 	// // считать размер массива
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
-	candies := []int{2, 3, 5, 1, 3}
-	extraCandies := 3
-	fmt.Println(kidsWithCandies(candies, extraCandies))
+	flowerbed := []int{1, 0, 0, 0, 1}
+	n := 1
+	// Output: true
+	fmt.Println(canPlaceFlowers(flowerbed, n))
 }
 
-func kidsWithCandies(candies []int, extraCandies int) []bool {
-	ans := []bool{}
-	for i := 0; i < len(candies); i++ {
-		if candies[i]+extraCandies > candies[i] {
-			ans = append(ans, true)
-		} else {
-			ans = append(ans, false)
-		}
-	}
-	return ans
+func canPlaceFlowers(flowerbed []int, n int) bool {
+
 }
