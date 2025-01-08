3e2ae8c69f75429115bbbce28446eeffe342393b
MG_Kalinin <1953115@gmail.com>
2025-01-08 21:48:33 +0300

    add 1431. Kids With the Greatest Number of Candies

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-// A. Быстрый поиск в массиве
-// https://contest.yandex.ru/contest/29188/problems/A/
+import "fmt"
+
+// 1431. Kids With the Greatest Number of Candies
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
 	// // считать размер массива
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
+	candies := []int{2, 3, 5, 1, 3}
+	extraCandies := 3
+	fmt.Println(kidsWithCandies(candies, extraCandies))
+}
 
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	ans := []bool{}
+	for i := 0; i < len(candies); i++ {
+		if candies[i]+extraCandies > candies[i] {
+			ans = append(ans, true)
+		} else {
+			ans = append(ans, false)
+		}
+	}
+	return ans
 }
