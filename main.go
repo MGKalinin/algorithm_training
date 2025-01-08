892e8617f5cfc2f2b8915a7361cff6c4dd16d1c8
MG_Kalinin <1953115@gmail.com>
2025-01-09 00:50:42 +0300

    605. Can Place Flowers false...

--- main.go
+++ main.go
@@ -10,12 +10,30 @@ func main() {
 	// // считать размер массива
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
-	flowerbed := []int{1, 0, 0, 0, 1}
-	n := 1
+
+	// flowerbed := []int{1, 0, 0, 0, 1}
+	// n := 1
 	// Output: true
+	// flowerbed := []int{1, 0, 0, 0, 1}
+	// n := 2
+	// Output: false
+	flowerbed := []int{1, 0, 0, 0, 0, 1}
+	n := 2
+	// false
 	fmt.Println(canPlaceFlowers(flowerbed, n))
 }
 
 func canPlaceFlowers(flowerbed []int, n int) bool {
-
+	for i := 1; i < len(flowerbed)-1; i++ {
+		if flowerbed[i-1] == 0 && flowerbed[i+1] == 0 {
+			n -= 1
+		} else {
+			continue
+		}
+	}
+	if n == 0 {
+		return true
+	} else {
+		return false
+	}
 }
