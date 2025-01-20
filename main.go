540e4224a40847f55bb04fc61f0af9049d788a10
MG_Kalinin <1953115@gmail.com>
2025-01-20 21:10:45 +0300

    add add

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 // 392. Is Subsequence
@@ -16,8 +17,8 @@ func main() {
 
 	// решить через многопоточность
 
-	s := "abc"
-	t := "ahbgdc"
+	s := "abc"    //искомое
+	t := "ahbgdc" //где ищем
 	// Output: true
 	// поделить искомую подпоследовательность на два-запустить две горутины
 
@@ -27,5 +28,37 @@ func main() {
 }
 
 func isSubsequence(s string, t string) bool {
+	mid := len(t) / 2
+	// fmt.Println(t[:mid])
+	// fmt.Println(t[mid:])
+	res := make([]rune, len(t[:mid]))
+	type indexStore struct {
+		ind int
+		val rune
+	}
+	ch1 := make(chan indexStore, len(t[:mid])) //для 1-й половины
+	// ch2 := make(chan rune, len(t[mid:])) //для 2-й половины
 
+	var wg sync.WaitGroup
+
+	for _, i := range t[:mid] { //идём по первой половине
+		wg.Add(1)
+		go func(i rune) {
+			for _, j := range s { //искомое
+				if j == i {
+					ch1 <- j
+				}
+
+			}
+		}(i)
+	}
+	wg.Wait()
+	go func() {
+		close(ch1)
+	}()
+
+	for i, k := range ch1 {
+		res[i] = k
+	}
+	return true
 }
