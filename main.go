e210cd328044a336ea15c654110c191ce9f9f296
MG_Kalinin <1953115@gmail.com>
2025-01-22 14:47:10 +0300

    huinia

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
+	"time"
 )
 
 // 392. Is Subsequence
@@ -23,58 +25,40 @@ func main() {
 
 	fmt.Println(isSubsequence(s, t))
 	// Получаем количество горутин
-	// fmt.Println("Number of goroutines:", runtime.NumGoroutine())
+	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
 func isSubsequence(s string, t string) bool {
-	// start := time.Now()
+	start := time.Now()
 
-	mid := len(t) / 2
-	// fmt.Println(t[:mid])
-	// fmt.Println(t[mid:])
 	res := make([]string, len(s))
 	type indexStore struct {
 		ind int
 		val string
 	}
-	ch1 := make(chan indexStore, len(t[:mid])) //для 1-й половины
-	ch2 := make(chan indexStore, len(t[mid:])) //для 2-й половины
+	ch := make(chan indexStore, len(s))
 
 	var wg sync.WaitGroup
-	wg.Add(2)
-	// Горутина для первой половины
+	wg.Add(100)
+
 	go func() {
 		defer wg.Done()
-		for _, char := range t[:mid] {
+		for _, char := range t {
 			for k, j := range s {
 				if j == char {
-					ch1 <- indexStore{ind: k, val: string(j)}
+					ch <- indexStore{ind: k, val: string(j)}
 				}
 			}
 		}
-		close(ch1)
+		close(ch)
 	}()
 
-	// Горутина для второй половины
 	go func() {
-		defer wg.Done()
-		for _, char := range t[mid:] {
-			for k, j := range s {
-				if j == char {
-					ch2 <- indexStore{ind: k, val: string(j)}
-				}
-			}
-		}
-		close(ch2)
+		wg.Wait()
 	}()
-	wg.Wait()
 
-	for item := range ch1 {
+	for item := range ch {
 		res[item.ind] = item.val
-	}
-	for item := range ch2 {
-		res[item.ind] = item.val
-		fmt.Println(res)
 	}
 
 	// Сравнение полученного результата с исходной строкой s
@@ -85,27 +69,8 @@ func isSubsequence(s string, t string) bool {
 	// fmt.Println("Result:", result)
 
 	// Замер времени выполнения
-	// elapsed := time.Since(start)
-	// fmt.Printf("Execution time: %s\n", elapsed)
-	if result == s {
-		return true
-	} else {
-		return false
-	}
+	elapsed := time.Since(start)
+	fmt.Printf("Execution time: %s\n", elapsed)
+	return result == s
 
 }
-
-// "mdadm": "4.1-11"
-//   "parted": "3.4-1"
-// POST partnumbers-2025-01-21/_doc
-// {
-//     "@timestamp": "2025-01-21T12:00:03",
-//     "AQPartNumber": "70079872",
-//     "device_id": "test624",
-//     "components": [
-//       {
-//         "AQPartNumber": "10088693",
-//         "device_id": "ANS685V4000124"
-//       }
-//     ]
-// }
