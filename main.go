4237d92371238be5ea88f36a153320469c5c136a
MG_Kalinin <1953115@gmail.com>
2025-01-25 13:08:35 +0300

    фвв фвв

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
-// 392. Is Subsequence
-// https://leetcode.com/problems/is-subsequence/description/?envType=problem-list-v2&envId=29p0sxl6
+// 1672. Richest Customer Wealth
+// https://leetcode.com/problems/richest-customer-wealth/description/?envType=problem-list-v2&envId=29p0sxl6
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
@@ -15,36 +17,52 @@ func main() {
 
 	// решить через многопоточность
 
-	s := "bb"     //искомое
-	t := "ahbgdc" //где ищем
-	// Output: false
-
-	fmt.Println(isSubsequence(s, t))
-	// Получаем количество горутин
-	// fmt.Println("Number of goroutines:", runtime.NumGoroutine())
-}
-
-func isSubsequence(s string, t string) bool {
-	// start := time.Now()
-	//одна горутина ищет первую половину s, вторая -вторую?
-	ans := ""
-	for _, v := range t {
-		for _, z := range s {
-			if z == v {
-				ans += string(v)
-			}
-		}
+	accounts := [][]int{
+		{1, 2, 3},
+		{3, 2, 1},
 	}
 
-	fmt.Println(ans)
+	fmt.Println(maximumWealth(accounts))
+	// Получаем количество горутин
+	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
+}
+
+func maximumWealth(accounts [][]int) int {
+	// start := time.Now()
+
+	var wg sync.WaitGroup
+	n := len(accounts)
+	res := make([]int, n)
+	ch := make(chan int, n)
+	for _, i := range accounts {
+		// fmt.Println(i)
+		wg.Add(1)
+		go func(i []int) {
+			wg.Done()
+			summ := 0
+			for _, k := range i {
+				summ += k
+			}
+			ch <- summ
+		}(i)
+	}
+	wg.Wait()
+	go func() {
+		close(ch)
+	}()
+	for i := range ch {
+		res = append(res, i)
+	}
+	maxx := 0
+	for _, i := range res {
+		if i > maxx {
+			maxx = i
+		}
+	}
 	// Замер времени выполнения
 	// elapsed := time.Since(start)
 	// fmt.Printf("Execution time: %s\n", elapsed)
-	return ans == s
 
+	// fmt.Println(maxx)
+	return maxx
 }
-
-// 2025-01-22T10:00:00
-// func newSeach(s string, t string) bool {
-
-// }
