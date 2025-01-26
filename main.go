d7d44aea56c6ef298843b8cb220e14612b8670b8
MG_Kalinin <1953115@gmail.com>
2025-01-26 16:02:20 +0300

    ready

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strings"
 	"sync"
 )
 
-// 1672. Richest Customer Wealth
-// https://leetcode.com/problems/richest-customer-wealth/description/?envType=problem-list-v2&envId=29p0sxl6
+// 1704. Determine if String Halves Are Alike
+// https://leetcode.com/problems/determine-if-string-halves-are-alike/description/?envType=problem-list-v2&envId=29p0sxl6
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
@@ -17,14 +18,51 @@ func main() {
 
 	// решить через многопоточность
 
-	accounts := [][]int{
-		{1, 2, 3},
-		{3, 2, 1},
-	}
+	s := "textbook"
+	// Output: true
 
-	fmt.Println(maximumWealth(accounts))
+	fmt.Println(halvesAreAlike(s))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
+}
+
+func halvesAreAlike(s string) bool {
+	// start := time.Now()
+	vowels := "aeiouAEIOU"
+	count1 := 0
+	count2 := 0
+	mid := len(s) / 2
+	var mux sync.Mutex
+	var wg sync.WaitGroup
+	for _, i := range s[:mid] {
+		wg.Add(1)
+		go func(i rune) {
+			defer wg.Done()
+			if strings.ContainsRune(vowels, i) {
+				mux.Lock()
+				count1++
+				mux.Unlock()
+			}
+		}(i)
+	}
+
+	for _, i := range s[mid:] {
+		wg.Add(1)
+		go func(i rune) {
+			defer wg.Done()
+			if strings.ContainsRune(vowels, i) {
+				mux.Lock()
+				count2++
+				mux.Unlock()
+			}
+		}(i)
+	}
+	wg.Wait()
+	// elapsed := time.Since(start)
+	// fmt.Printf("Execution time: %s\n", elapsed)
+
+	return count1 == count2
+
 }
 
 func maximumWealth(accounts [][]int) int {
