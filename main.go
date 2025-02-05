d21ce9a74b4ff09316a5e27c2c54af2ac1b8bde3
MG_Kalinin <1953115@gmail.com>
2025-02-06 00:58:55 +0300

    add 70. Climbing Stairs

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"sync"
 )
 
-// 36. Valid Sudoku // TODO: solved
-// https://leetcode.com/problems/valid-sudoku/description/?envType=problem-list-v2&envId=29p0sxl6
+// 70. Climbing Stairs
+// https://leetcode.com/problems/climbing-stairs/description/?envType=problem-list-v2&envId=dynamic-programming
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
@@ -15,95 +15,18 @@ func main() {
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
 
-	// решить через многопоточность
-	board := [][]byte{
-		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
-	}
-	// Output: true
+	n := 3
+	//Output: 3
+	//Explanation: There are three ways to climb to the top.
+	//	1. 1 step + 1 step + 1 step
+	//	2. 1 step + 2 steps
+	//	3. 2 steps + 1 step
 
-	fmt.Println(isValidSudoku(board))
+	fmt.Println(climbStairs(n))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
-func isValidSudoku(board [][]byte) bool {
-	var wg sync.WaitGroup
-	ch := make(chan bool, 3)
-	for i := 0; i < len(board); i++ {
-		wg.Add(3)
-		//проверка строки
-		go func(row int) {
-			defer wg.Done()
-			if !checkUnique(board[row]) {
-				ch <- false
-				return
-			}
-			ch <- true
-		}(i)
-		// проверка столбца
-		go func(col int) {
-			defer wg.Done()
-			column := make([]byte, 9)
-			for j := 0; j < 9; j++ {
-				column[j] = board[j][col]
-			}
-			if !checkUnique(column) {
-				ch <- false
-				return
-			}
-			ch <- true
-		}(i)
-		//	проверка блоков 3*3
-		go func(block int) {
-			defer wg.Done()
-			rowBase := (block / 3) * 3
-			colBase := (block % 3) * 3
-			subBox := make([]byte, 9)
-			index := 0
-			for r := rowBase; r < rowBase+3; r++ {
-				for c := colBase; c < colBase+3; c++ {
-					subBox[index] = board[r][c]
-					index++
-				}
-			}
-			if !checkUnique(subBox) {
-				ch <- false
-				return
-			}
-			ch <- true
-		}(i)
-	}
+func climbStairs(n int) int {
 
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-	for result := range ch {
-		if !result {
-			return false
-		}
-	}
-	return true
-}
-
-// функция проверки byte слайса
-func checkUnique(items []byte) bool {
-	checkmap := make(map[byte]bool)
-	for _, v := range items {
-		if v != '.' {
-			if checkmap[v] {
-				return false
-			}
-			checkmap[v] = true
-		}
-	}
-	return true
 }
