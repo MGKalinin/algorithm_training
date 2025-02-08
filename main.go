37af4cf0e6a8bb6337f09878bad0963e07801be4
MG_Kalinin <1953115@gmail.com>
2025-02-08 21:21:26 +0300

    add 746. Min Cost Climbing Stairs

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"sync"
 )
 
-// 118. Pascal's Triangle
-// https://leetcode.com/problems/pascals-triangle/description/?envType=problem-list-v2&envId=dynamic-programming
+// 746. Min Cost Climbing Stairs
+// https://leetcode.com/problems/min-cost-climbing-stairs/description/?envType=problem-list-v2&envId=dynamic-programming
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
@@ -15,12 +14,12 @@ func main() {
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
 
-	numRows := 5
-	//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-	fmt.Println(generate(numRows))
+	cost := []int{10, 15, 20}
+	//Output: 15
+	fmt.Println(minCostClimbingStairs(cost))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
-func generate(numRows int) [][]int {
+func minCostClimbingStairs(cost []int) int {
 
 }
