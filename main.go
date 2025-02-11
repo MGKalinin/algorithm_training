0cee9adfb5d8287c87c25891801af8b135770215
MG_Kalinin <1953115@gmail.com>
2025-02-11 11:51:05 +0300

    653. Two Sum IV - Input is a BST

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"runtime"
 )
 
-// 1641. Count Sorted Vowel Strings
-// https://leetcode.com/problems/count-sorted-vowel-strings/description/?envType=problem-list-v2&envId=dynamic-programming
+// 653. Two Sum IV - Input is a BST
+// https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/?envType=problem-list-v2&envId=binary-search-tree
 
 func main() {
 	// reader := bufio.NewReader(os.Stdin)
@@ -14,17 +14,23 @@ func main() {
 	// line, _ := reader.ReadString('\n')
 	// line = strings.TrimSpace(line) // Убираем символ новой строки и пробелы
 
-	n := 2
-	//Output: 15
-	//Explanation: The 15 sorted strings that consist of vowels only are
-	//["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
-	//	Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
+	root = []int{5, 3, 6, 2, 4, null, 7}
+	k := 9
+	//Output: true
 
-	fmt.Println(countVowelStrings(n))
+	fmt.Println(findTarget(root, k))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
-func countVowelStrings(n int) int {
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findTarget(root *TreeNode, k int) bool {
 
 }
