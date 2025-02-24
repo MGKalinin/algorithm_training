d016d05210ad2f1b2a9d64b4ab720a8d1d47f4d7
MG_Kalinin <1953115@gmail.com>
2025-02-24 22:58:46 +0300

    add some ...1200. Minimum Absolute Difference

--- main.go
+++ main.go
@@ -19,13 +19,11 @@ func main() {
 func maxProfit(prices []int) int {
 	n := len(prices)
 	dp := make([]int, n+1)
-	//fmt.Println(dp)
-	dp[1] = prices[0]
-	//fmt.Println(dp)
-	for i := 2; i <= n; i++ {
-		dp[i] = dp[i-1] - prices[i-1]
+	for i := 0; i < n; i++ {
+		if prices[i] < prices[i+1] {
+			//dp[i]=
+		}
 	}
-	fmt.Println(dp)
 	return dp[n]
 }
 
