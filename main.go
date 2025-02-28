64d9833dfb800aa6037d84f15d19b1df662a94d2
MG_Kalinin <1953115@gmail.com>
2025-03-01 00:47:26 +0300

    add 395. Longest Substring with At Least K Repeating Characters

--- main.go
+++ main.go
@@ -5,19 +5,25 @@ import (
 	"runtime"
 )
 
-// 5. Longest Palindromic Substring
-// https://leetcode.com/problems/longest-palindromic-substring/description/?envType=problem-list-v2&envId=dynamic-programming
+// 395. Longest Substring with At Least K Repeating Characters
+// https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/description/?envType=problem-list-v2&envId=29p0sxl6
 
 func main() {
-	s := "babad"
-	//Output: "bab"
-	//Explanation: "aba" is also a valid answer.
-
+	s := "aaabb"
+	k := 3
+	//Output: 3
+	//Explanation: The longest substring is "aaa", as 'a' is repeated 3 times.
 	fmt.Println(longestPalindrome(s))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
-func longestPalindrome(s string) string {
+func longestSubstring(s string, k int) int {
+	//Вот пошаговый алгоритм:
+	//Подсчитать частоту символов в строке.
+	//	Найти символы, которые встречаются реже, чем k. Они разделят строку на подстроки.
+	//	Запустить горутину для обработки каждой подстроки параллельно.
+	//	Использовать каналы для сбора результатов.
+	//	Вернуть максимальную длину подходящей подстроки.
 
 }
