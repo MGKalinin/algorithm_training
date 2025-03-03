e815a001032cd88b4cdb7366e6a64691ef891dc1
MG_Kalinin <1953115@gmail.com>
2025-03-03 23:33:12 +0300

    add 151. Reverse Words in a String

--- main.go
+++ main.go
@@ -3,24 +3,21 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strings"
 )
 
-// 345. Reverse Vowels of a String
-// https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=study-plan-v2&envId=leetcode-75
+// 151. Reverse Words in a String
+// https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=leetcode-75
 
 func main() {
-	s := "IceCreAm"
-	//Output: "AceCreIm"
-	//Explanation:
-	//	The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+	s := "the sky is blue"
+	//Output: "blue is sky the"
 
-	fmt.Println(reverseVowels(s))
+	fmt.Println(reverseWords(s))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
-func reverseVowels(s string) string {
-	vowels := []rune{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-	start, end := 0, len(s)
-if s[start] && s[end]
+func reverseWords(s string) string {
+
 }
