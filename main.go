6a0274be14bbbee2e367cf3569949f9685fff998
MG_Kalinin <1953115@gmail.com>
2025-03-07 01:50:00 +0300

    add 416. Partition Equal Subset Sum

--- main.go
+++ main.go
@@ -6,21 +6,18 @@ import (
 	"strings"
 )
 
-// 674. Longest Continuous Increasing Subsequence
-// https://leetcode.com/problems/longest-continuous-increasing-subsequence/description/?envType=problem-list-v2&envId=27tz3m7t
+// 416. Partition Equal Subset Sum
+// https://leetcode.com/problems/partition-equal-subset-sum/description/?envType=problem-list-v2&envId=27tz3m7t
 
 func main() {
-	nums := []int{1, 3, 5, 4, 7}
-	//Output: 3
+	nums := []int{1, 5, 11, 5}
+	//true
 
-	fmt.Println(findLengthOfLCIS(nums))
+	fmt.Println(canPartition(nums))
 	// Получаем количество горутин
 	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
-func findLengthOfLCIS(nums []int) int {
-	dp := make([]int, len(nums)+1)
-	for i := 0; i <= len(dp); i++ {
+func canPartition(nums []int) bool {
 
-	}
 }
