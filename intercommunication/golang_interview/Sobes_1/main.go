549f28e6d9eed732cdd33d62f6b99020e99466db
MG_Kalinin <1953115@gmail.com>
2025-05-10 12:25:34 +0300

    first problem has been solved

--- intercommunication/golang_interview/Sobes_1/main.go
+++ intercommunication/golang_interview/Sobes_1/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"sort"
 )
 
 //
@@ -11,45 +11,53 @@ import (
 //Что выведет код? Исправить все проблемы
 //==========================================
 
-func main() {
-	ch := make(chan int)
-	wg := &sync.WaitGroup{}
-	wg.Add(3)
-	for i := 0; i < 3; i++ {
-		go func(v int) {
-			defer wg.Done()
-			ch <- v * v
-		}(i)
-	}
-	wg.Wait()
-	var sum int
-	for v := range ch {
-		sum += v
-	}
-	fmt.Printf("result: %d\n", sum)
-}
+//func main() {
+//	ch := make(chan int) //добавить буфер канала 3
+//	wg := &sync.WaitGroup{}
+//	wg.Add(3)
+//
+//	// горутина закрытия канала после завершения всех писателей
+//	go func() {
+//		wg.Wait()
+//		close(ch)
+//	}()
+//
+//	for i := 0; i < 3; i++ {
+//		go func(v int) {
+//			defer wg.Done()
+//			ch <- v * v // 0,1,2
+//		}(i)
+//	}
+//	//wg.Wait()
+//	//close(ch) //закрыть канал
+//	var sum int
+//	for v := range ch {
+//		sum += v
+//	}
+//	fmt.Printf("result: %d\n", sum) //0,1,4
+//}
 
 //
 //==========================================
 //Задача 2
 //Что выведет код?
 //==========================================
-//
-//func main() {
-//v := []int{3, 4, 1, 2, 5}
-//ap(v)
-//sr(v)
-//fmt.Println(v)
-//}
-//
-//func ap(arr []int) {
-//arr = append(arr, 10)
-//}
-//
-//func sr(arr []int) {
-//sort.Ints(arr)
-//}
-//
+
+func main() {
+	v := []int{3, 4, 1, 2, 5}
+	ap(v)
+	sr(v)
+	fmt.Println(v)
+}
+
+func ap(arr []int) {
+	arr = append(arr, 10)
+}
+
+func sr(arr []int) {
+	sort.Ints(arr)
+}
+
 //==========================================
 //Задача 3
 //Что выведет код?
