e7dbe13f538b17dd83a867a4a08f798f2377dbf9
MG_Kalinin <1953115@gmail.com>
2025-05-11 16:26:41 +0300

    add 11. Container With Most Water

--- LeetCode75/Two_Pointers/11ContainerWithMostWater/main.go
+++ LeetCode75/Two_Pointers/11ContainerWithMostWater/main.go
@@ -1,1 +1,13 @@
-package _1ContainerWithMostWater
+package main
+
+func main() {
+	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	//Output: 49
+	maxArea(height)
+}
+
+func maxArea(height []int) int {
+	maxVal := 0        //максимальный объём
+	start, end := 0, 0 //левый, правый край контейнера
+
+}
