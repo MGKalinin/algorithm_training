de934d0bde2b44288cf904ad1df457a4777691dc
MG_Kalinin <1953115@gmail.com>
2025-06-12 17:47:26 +0300

    add 2215 decided 724

---
+++ LeetCode75/HashMapSet/2215FindDifferenceTwoArrays/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func findDifference(nums1 []int, nums2 []int) [][]int {
+
+}
+
+func main() {
+	nums1 := []int{1, 2, 3}
+	nums2 := []int{2, 4, 6}
+	fmt.Println(findDifference(nums1, nums2))
+}
