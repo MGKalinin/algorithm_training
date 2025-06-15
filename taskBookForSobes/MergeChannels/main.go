5fef2354d21f41dbbc4407743f997d300f0d5cdd
MG_Kalinin <1953115@gmail.com>
2025-06-15 20:32:42 +0300

    add some changed in task merge channels

--- taskBookForSobes/MergeChannels/main.go
+++ taskBookForSobes/MergeChannels/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
+func main() {
+	for item := range merge(...){
+		// ...
+	}
+	fmt.Println("ok")
+}
+
 // Написать код функции, которая делает merge N каналов. Весь входной поток
 // перенаправляется в один канал.
 func merge(cs ...<-chan int) <-chan int {
