4593b546c7c01f3d944ba2da03c9d2e3e40daebe
MG_Kalinin <1953115@gmail.com>
2025-06-16 15:02:30 +0300

    add some changed to set of links

--- taskBookForSobes/setOfLinks/main.go
+++ taskBookForSobes/setOfLinks/main.go
@@ -1,1 +1,17 @@
 package main
+
+//Интерактивная задача на кодинг, приближенный к условиям
+//работы
+
+func main() {
+	var urls = []string{
+		"http://ozon.ru",
+		"https://ozon.ru",
+		"http://google.com",
+		"http://somesite.com",
+		"http://non-existent.domain.tld",
+		"https://ya.ru",
+		"http://ya.ru",
+		"http://ёёёё",
+	}
+}
