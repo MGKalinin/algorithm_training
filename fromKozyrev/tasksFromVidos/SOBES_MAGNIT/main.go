eb6ce401d6772513a056596063fd728ca0ff83bb
MG_Kalinin <1953115@gmail.com>
2025-07-01 11:59:14 +0300

    decided SOBES_MAGNIT tasks

--- fromKozyrev/tasksFromVidos/SOBES_MAGNIT/main.go
+++ fromKozyrev/tasksFromVidos/SOBES_MAGNIT/main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"sync"
 )
 
 // 1. Смерджить 2 упорядоченных массива в 1 упорядоченный массив.
 // Пример:
 // дано: [1, 2, 3, 4, 5], [4, 5, 6, 7,8]
 // Результат: [1, 2, 3, 4, 4, 5, 5, 6, 7, 8]
-// TODO решить в песочнице
-func merge(a []int, b []int) []int {
+// TO DO решить в песочнице https://go.dev/play/p/SR093pFtjX1
 
+func merge(a []int, b []int) []int {
+	n := len(a) + len(b)
+	ans := make([]int, 0, n)
+
+	i, j := 0, 0
+
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			ans = append(ans, a[i])
+			i++
+		} else {
+			ans = append(ans, b[j])
+			j++
+		}
+	}
+
+	//for ; i < len(a); i++ {
+	//ans = append(ans, a[i])
+	//}
+
+	//for ; j < len(b); j++ {
+	//ans = append(ans, b[j])
+	//}
+
+	ans = append(ans, a[i:]...)
+	//result = append(result, a[i:]...)
+	//Эта конструкция делает следующее:
+	//a[i:] - создает новый срез (slice), который содержит:
+	//Все элементы исходного среза a
+	//Начиная с индекса i
+	//До конца среза
+	//... - оператор распаковки (variadic operator). Он "распаковывает" элементы среза, передавая их как отдельные аргументы.
+	//	append - добавляет распакованные элементы в конец среза result
+	ans = append(ans, b[j:]...)
+
+	fmt.Println(ans)
+	return ans
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5}
+	b := []int{4, 5, 6, 7, 8}
+	fmt.Println(merge(a, b))
 }
 
 // 2. Смерджить произвольное количество каналов в один канал
