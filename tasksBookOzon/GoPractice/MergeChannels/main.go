84c61e4c460c39ae4736da7956930e837ff451eb
MG_Kalinin <1953115@gmail.com>
2025-07-09 16:17:28 +0300

    MergeChannels decided

--- tasksBookOzon/GoPractice/MergeChannels/main.go
+++ tasksBookOzon/GoPractice/MergeChannels/main.go
@@ -1,11 +1,52 @@
 package main
 
-//TODO код писать в песочнице
+import (
+	"fmt"
+	"sync"
+)
+
+//TO DO код писать в песочнице
 
 // Написать код функции, которая делает merge N каналов. Весь входной поток
 // перенаправляется в один канал.
 func merge(cs ...<-chan int) <-chan int {
-
+	result := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(len(cs))
+	for _, ch := range cs {
+		go func(c <-chan int) {
+			for val := range c {
+				result <- val
+			}
+			wg.Done()
+		}(ch)
+	}
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+	return result
 }
 
-// https://go.dev/play/p/csG5tIbSFTb
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	merged := merge(ch1, ch2)
+
+	go func() {
+		ch1 <- 1
+		ch1 <- 2
+		close(ch1)
+
+	}()
+	go func() {
+		ch2 <- 3
+		ch2 <- 4
+		close(ch2)
+	}()
+
+	for val := range merged {
+		fmt.Println(val)
+	}
+}
