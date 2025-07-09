f1869114427f9dd56d6714cc7d79b5564563dbb8
MG_Kalinin <1953115@gmail.com>
2025-07-09 18:59:26 +0300

    finish version

--- tasksBookOzon/GoPractice/MergeChannels/main.go
+++ tasksBookOzon/GoPractice/MergeChannels/main.go
@@ -5,8 +5,6 @@ import (
 	"sync"
 )
 
-//TODO см через select https://go.dev/play/p/r1b3F09juuM
-
 // Написать код функции, которая делает merge N каналов. Весь входной поток
 // перенаправляется в один канал.
 func merge(cs ...<-chan int) <-chan int {
