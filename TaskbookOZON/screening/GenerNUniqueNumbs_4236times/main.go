63e154b646b3462926afb32140a39fe581f82da3
MG_Kalinin <1953115@gmail.com>
2025-07-31 10:34:37 +0300

    add task

---
+++ TaskbookOZON/screening/GenerNUniqueNumbs_4236times/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+//Требуется реализовать функцию uniqRandn, которая генерирует слайс длины n уникальных,
+//рандомных чисел.
+
+func main() {
+	fmt.Println(uniqRandn(10))
+}
+
+func uniqRandn(n int) []int {
+	//...
+}
+
+// https://go.dev/play/p/SQCZ6bTxIMd
