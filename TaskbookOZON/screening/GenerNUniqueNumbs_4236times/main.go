8d121069f645b3289d1f8f74bd03cdcc3b432292
MG_Kalinin <1953115@gmail.com>
2025-07-31 15:02:05 +0300

    decided GenerNUniqueNumbs_4236times

--- TaskbookOZON/screening/GenerNUniqueNumbs_4236times/main.go
+++ TaskbookOZON/screening/GenerNUniqueNumbs_4236times/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 //Требуется реализовать функцию uniqRandn, которая генерирует слайс длины n уникальных,
@@ -13,7 +14,17 @@ func main() {
 }
 
 func uniqRandn(n int) []int {
-	//...
+	result := make([]int, 0, n)      //инициализация slice длиной 0 и ёмкость n; инициализация slice без 0
+	rand.Seed(time.Now().UnixNano()) // инициализация генератора
+	unic := make(map[int]struct{})
+	for len(result) < n {
+		r := rand.Int()            // случайное int
+		if _, ok := unic[r]; !ok { // value, ok
+			unic[r] = struct{}{}
+			result = append(result, r)
+		}
+	}
+	return result
 }
 
 // https://go.dev/play/p/ggJxkffaR8j
