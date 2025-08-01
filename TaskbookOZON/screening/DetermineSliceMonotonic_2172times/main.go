7c794586e259b622141beddef579b5c7d3b662ef
MG_Kalinin <1953115@gmail.com>
2025-08-01 17:09:14 +0300

    finished func monotonic

--- TaskbookOZON/screening/DetermineSliceMonotonic_2172times/main.go
+++ TaskbookOZON/screening/DetermineSliceMonotonic_2172times/main.go
@@ -1,11 +1,32 @@
 package main
 
-//Является ли слайс монотонным?
-//Монотонная функция - функция одной переменной, определённая на некотором
-//подмножестве действительных чисел, которая либо везде (на области своего
-//определения) не убывает, либо везде не возрастает.
-//{1,7} - true
-//{1,1} - true
-//{3,3,1} - true
-//{9,5,1} - true
-//{23,5,23} - false
+import "fmt"
+
+// Является ли слайс монотонным?
+// Монотонная функция - функция одной переменной, определённая на некотором
+// подмножестве действительных чисел, которая либо везде (на области своего
+// определения) не убывает, либо везде не возрастает.
+// {1,7} - true
+// {1,1} - true
+// {3,3,1} - true
+// {9,5,1} - true
+// {23,5,23} - false
+func monotonic(in []int) bool {
+	isIncreasing, isDecreasing := true, true
+
+	return isIncreasing || isDecreasing
+
+}
+
+func main() {
+	a := []int{1, 7}      //- true
+	b := []int{1, 1}      //- true
+	c := []int{3, 3, 1}   //- true
+	d := []int{9, 5, 1}   //- true
+	e := []int{23, 5, 23} //- false
+	fmt.Println(monotonic(a))
+	fmt.Println(monotonic(b))
+	fmt.Println(monotonic(c))
+	fmt.Println(monotonic(d))
+	fmt.Println(monotonic(e))
+}
