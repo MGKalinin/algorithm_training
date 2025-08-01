0ce5388b69ebba57e10f399a44b47d2646cb89e4
MG_Kalinin <1953115@gmail.com>
2025-08-02 01:36:26 +0300

    ProblemWithSlices3_1859times_decided

--- TaskbookOZON/screening/ProblemWithSlices3_1859times_decided/main.go
+++ TaskbookOZON/screening/ProblemWithSlices3_1859times_decided/main.go
@@ -6,12 +6,12 @@ import "time"
 func main() {
 	timeStart := time.Now()
 	_, _ = <-worker(), <-worker()
-	println(int(time.Since(timeStart).Seconds()))
+	println(int(time.Since(timeStart).Seconds())) // 6 сек
 }
 func worker() chan int {
-	ch := make(chan int)
+	ch := make(chan int) // не буф канал
 	go func() {
-		time.Sleep(3 * time.Second)
+		time.Sleep(3 * time.Second) //3 сек 1 раз, потом 2 раз 3 сек
 		ch <- 1
 	}()
 	return ch
