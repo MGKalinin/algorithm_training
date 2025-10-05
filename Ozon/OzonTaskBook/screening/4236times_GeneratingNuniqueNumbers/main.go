fbb01a9c7a5eea8d81a9ffe87dd4b209d7e8845c
MG_Kalinin <1953115@gmail.com>
2025-10-05 21:28:32 +0300

    added task about generation digital

---
+++ Ozon/OzonTaskBook/screening/4236times_GeneratingNuniqueNumbers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+//Требуется реализовать функцию uniqRandn, которая генерирует слайс длины n уникальных,
+//рандомных чисел.
+
+func main() {
+
+	fmt.Println(uniqRandn(10))
+}
+
+func uniqRandn(n int) []int {
+
+}
