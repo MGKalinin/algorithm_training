f9aab918c48b526a45d681875f49424de9957023
MG_Kalinin <1953115@gmail.com>
2025-10-10 22:22:20 +0300

    added task from screening

---
+++ Ozon/OzonTaskBook/screening/2575times_SliceChallenge1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// Что выведет программа?
+func a() {
+	x := []int{}
+	x = append(x, 0)
+	x = append(x, 1)
+	x = append(x, 2)
+	y := append(x, 3)
+	z := append(x, 4)
+	fmt.Println(y, z)
+}
+func main() {
+}
